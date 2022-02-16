fa029d39a4fc826cb0546063ee27d427748bf3b2
Tanmaypatil123 <77950208+Tanmaypatil123@users.noreply.github.com>
2022-02-16 13:19:17 +0530

    frontend setup

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function homePage() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Welcome to nextjs app</h1>
+    </div>
+  );
+}
