fb1e1766483f9bb8b26f50a0e63eecbd51a9c342
Tanmaypatil123 <77950208+Tanmaypatil123@users.noreply.github.com>
2022-02-17 15:42:24 +0530

    removed unwanted contracts

--- test/unit/Video.test.js
+++ test/unit/Video.test.js
@@ -1,9 +1,8 @@
 const { assert, expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("Video contract", function() {
-
-  before(async function() {
+describe("Video contract", function () {
+  before(async function () {
     accounts = await ethers.getSigners();
     deployer = accounts[0];
     video_owner = accounts[1];
@@ -11,10 +10,9 @@ describe("Video contract", function() {
     const Videos = await ethers.getContractFactory("VideoContract");
     videos = await Videos.deploy();
     await videos.deployed();
-
   });
 
-  it("checks deployment", async function() {
+  it("checks deployment", async function () {
     const address = await videos.address;
     assert.notEqual(address, 0x0);
     assert.notEqual(address, "");
@@ -22,14 +20,14 @@ describe("Video contract", function() {
     assert.notEqual(address, undefined);
   });
 
-  it("should emit Video created event", async function() {
+  it("should emit Video created event", async function () {
     const cid = "Qmeff4AvGELYTWUZD23s36VrPo6An24wywFxoDskjzVPJc";
     const Comments = await ethers.getContractFactory("Comments");
-    comments = await  Comments.deploy(1)
-    await  comments.deployed()
-    const transaction = await videos.connect(video_owner).UploadVideo(
-      cid, "test title", "test desc",comments.address
-    );
+    comments = await Comments.deploy(1);
+    await comments.deployed();
+    const transaction = await videos
+      .connect(video_owner)
+      .UploadVideo(cid, "test title", "test desc", comments.address);
     const res = await transaction.wait();
     const hash = res.events[0].args;
     expect(transaction).to.emit(videos, "VideoCreated");
@@ -42,9 +40,10 @@ describe("Video contract", function() {
     expect(hash[6]).to.equal(video_owner.address);
   });
 
-  it("should add video in mapping", async function() {
+  it("should add video in mapping", async function () {
     const VideoCount = await videos.VideoCount();
-    const [ipfs, title, desc, likes, dislikes, tipamount, owner] = await videos.getVideoDetails(1);
+    const [ipfs, title, desc, likes, dislikes, tipamount, owner] =
+      await videos.getVideoDetails(1);
     expect(VideoCount).to.equal(1);
     expect(ipfs).to.equal("Qmeff4AvGELYTWUZD23s36VrPo6An24wywFxoDskjzVPJc");
     expect(title).to.equal("test title");
@@ -55,48 +54,55 @@ describe("Video contract", function() {
     expect(owner).to.equal(video_owner.address);
   });
 
-  it("should like the video", async function() {
+  it("should like the video", async function () {
     const likeTx = await videos.likesTheVideo(1);
     const likeRes = await likeTx.wait();
     const likeHash = likeRes.events[0].args;
-    const [ipfs2, title2, desc2, likes2, dislikes2, tipamount2, owner2] = await videos.getVideoDetails(1);
+    const [ipfs2, title2, desc2, likes2, dislikes2, tipamount2, owner2] =
+      await videos.getVideoDetails(1);
     expect(likes2).to.equal(1);
-    expect(likeHash[0]).to.equal("Qmeff4AvGELYTWUZD23s36VrPo6An24wywFxoDskjzVPJc");
+    expect(likeHash[0]).to.equal(
+      "Qmeff4AvGELYTWUZD23s36VrPo6An24wywFxoDskjzVPJc"
+    );
     expect(likeHash[1]).to.equal(1);
   });
 
-  it("should dislike the video",async function(){
-    const dislikeTx = await videos.DislikesTheVideo(1)
-    const dislikesRes = await dislikeTx.wait()
+  it("should dislike the video", async function () {
+    const dislikeTx = await videos.DislikesTheVideo(1);
+    const dislikesRes = await dislikeTx.wait();
     const dislikesHash = dislikesRes.events[0].args;
-    const [ipfs3, title3, desc3, likes3, dislikes3, tipamount3, owner3] = await videos.getVideoDetails(1);
-    expect(dislikes3).to.equal(1)
-    expect(dislikesHash[0]).to.equal("Qmeff4AvGELYTWUZD23s36VrPo6An24wywFxoDskjzVPJc")
-    expect(dislikesHash[1]).to.equal(1)
+    const [ipfs3, title3, desc3, likes3, dislikes3, tipamount3, owner3] =
+      await videos.getVideoDetails(1);
+    expect(dislikes3).to.equal(1);
+    expect(dislikesHash[0]).to.equal(
+      "Qmeff4AvGELYTWUZD23s36VrPo6An24wywFxoDskjzVPJc"
+    );
+    expect(dislikesHash[1]).to.equal(1);
   });
 
-  it("should tip video owner with 1 ether",async function(){
-    const transactionTip = await  videos.tipVideoOwner(1,{
-      value:ethers.utils.parseEther("1.0")
-    })
-    const tipRes = await transactionTip.wait()
-    const tipHash = await tipRes.events[0].args
-    const [ipfs4, title4, desc4, likes4, dislikes4, tipamount4, owner4] = await videos.getVideoDetails(1);
-    expect(tipHash[5]).to.equal(ethers.utils.parseEther("1.0"))
-    expect(tipamount4).to.equal(ethers.utils.parseEther("1.0"))
+  it("should tip video owner with 1 ether", async function () {
+    const transactionTip = await videos.tipVideoOwner(1, {
+      value: ethers.utils.parseEther("1.0"),
+    });
+    const tipRes = await transactionTip.wait();
+    const tipHash = await tipRes.events[0].args;
+    const [ipfs4, title4, desc4, likes4, dislikes4, tipamount4, owner4] =
+      await videos.getVideoDetails(1);
+    expect(tipHash[5]).to.equal(ethers.utils.parseEther("1.0"));
+    expect(tipamount4).to.equal(ethers.utils.parseEther("1.0"));
   });
 
-  it("should comment on video",async function(){
+  it("should comment on video", async function () {
     const ComTx = await comments.connect(user).CreateComment("great video");
-    const ComRes = await ComTx.wait()
-    const ComHash = await ComRes.events[0].args
+    const ComRes = await ComTx.wait();
+    const ComHash = await ComRes.events[0].args;
     expect(ComHash[0]).to.equal(1);
     expect(ComHash[1]).to.equal("great video");
     expect(ComHash[2]).to.equal(user.address);
   });
 
-  it("should add comment to mapping",async function(){
-    const [add , content] = await comments.getCommentsDetails(1);
+  it("should add comment to mapping", async function () {
+    const [add, content] = await comments.getCommentsDetails(1);
     expect(add).to.equal(user.address);
     expect(content).to.equal("great video");
   });
